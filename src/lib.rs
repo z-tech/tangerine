a75361f0bf42417fff35140fe526dc93cf9919af
Andrew Zitek <zitek936@gmail.com>
2021-06-29 18:42:13 -0400

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ use std::io::Write;
 use std::{thread, thread::{JoinHandle}};
 
 use crypto_hash::{Algorithm, Hasher};
-use miller_rabin::{is_prime};
-// use modinverse::egcd;
 use num_bigint::{BigUint, RandBigInt};
 use rand::Rng;
 
@@ -24,14 +22,81 @@ fn hash_byte_sequence(bytes: &[u8]) -> Vec<u8> {
     hasher.finish()
 }
 
+fn miller_rabin(candidate: &BigUint) -> bool {
+    let mut d: BigUint = candidate.clone() - BigUint::from_bytes_be(b"1");
+    let mut t: BigUint = BigUint::from_bytes_be(b"0");
+    while d.modpow(&BigUint::from_bytes_be(b"1"), &BigUint::from_bytes_be(b"2")) == BigUint::from_bytes_be(b"0") {
+        d /= BigUint::from_bytes_be(b"2");
+        t += BigUint::from_bytes_be(b"1");
+    }
+
+    for _trial in 0..5 {
+        let mut rng = rand::thread_rng(); // thread-local random generator seeded by system: https://docs.rs/rand/0.8.4/rand/fn.thread_rng.html
+        let a: BigUint = rng.gen_biguint_range(&BigUint::from_bytes_be(b"2"), &(candidate - BigUint::from_bytes_be(b"1")));
+        let mut v: BigUint = a.modpow(&d, &BigUint::from_bytes_be(b"0"));
+        if v != BigUint::from_bytes_be(b"0") {
+            let mut i: BigUint = BigUint::from_bytes_be(b"0");
+            while v != (d.clone() - BigUint::from_bytes_be(b"1")) {
+                if i == t.clone() - BigUint::from_bytes_be(b"1") {
+                    false;
+                } else {
+                    i = i + BigUint::from_bytes_be(b"1");
+                    v = v.modpow(&BigUint::from_bytes_be(b"2"), &candidate);
+                }
+            }
+        }
+
+    }
+    return true;
+}
+
+fn is_prime(candidate: &BigUint) -> bool {
+    let zero: BigUint = BigUint::from_bytes_be(b"0");
+    let one: BigUint = BigUint::from_bytes_be(b"1");
+
+    // if less than two, not prime
+    if *candidate == zero || *candidate == one {
+        false;
+    }
+
+    let primes: Vec<u64> = vec![
+        2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61,
+        67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137,
+        139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211,
+        223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283,
+        293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379,
+        383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461,
+        463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563,
+        569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643,
+        647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739,
+        743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829,
+        839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937,
+        941, 947, 953, 967, 971, 977, 983, 991, 997
+    ];
+
+    // eliminate a good deal of candidates by checking first hundred or so primes
+    for prime in primes.iter() {
+        // make the prime into a BigUint
+        let prime_bytes: Vec<u8> = prime.to_be_bytes().to_vec();
+        let prime_biguint: BigUint = BigUint::from_bytes_be(&prime_bytes);
+
+        // if the candidate is divisible by the prime, candidate is not a prime
+        if candidate.modpow(&one, &prime_biguint) == zero {
+            false;
+        }
+    }
+
+    return miller_rabin(&candidate);
+}
+
 fn get_prime(size_in_bits: usize) -> BigUint {
     let mut rng = rand::thread_rng(); // thread-local random generator seeded by system: https://docs.rs/rand/0.8.4/rand/fn.thread_rng.html
-    let candidate: BigUint = rng.gen_biguint(size_in_bits as u64);
+    let mut candidate: BigUint = rng.gen_biguint(size_in_bits as u64);
     loop {
-        if is_prime(&candidate, size_in_bits) {
-            candidate;
+        if is_prime(&candidate) {
+            candidate.clone();
         }
-        candidate += 1;
+        candidate += BigUint::from_bytes_be(b"1");
     }
 }
 
@@ -39,10 +104,10 @@ fn get_distinct_primes(size_in_bits: usize) -> (BigUint, BigUint) {
     let join_handle_a: JoinHandle<BigUint> = thread::spawn(move || get_prime(size_in_bits));
     let join_handle_b: JoinHandle<BigUint> = thread::spawn(move || get_prime(size_in_bits));
     let a: BigUint = join_handle_a.join().unwrap();
-    let b: BigUint = join_handle_a.join().unwrap();
+    let mut b: BigUint = join_handle_b.join().unwrap();
     loop {
         if a != b {
-            (a, b);
+            (a.clone(), b);
         }
         b = get_prime(size_in_bits);
     }
@@ -50,13 +115,12 @@ fn get_distinct_primes(size_in_bits: usize) -> (BigUint, BigUint) {
 
 fn hash_value_to_prime(value: &[u8], nonce: &[u8]) -> BigUint {
     let hashed_value_and_nonce: Vec<u8> = [value.to_vec(), nonce.to_vec()].concat();
-    let candidate: BigUint = BigUint::from_bytes_be(&hashed_value_and_nonce);
-    let size_in_bits: usize = 256;
+    let mut candidate: BigUint = BigUint::from_bytes_be(&hashed_value_and_nonce);
     loop {
-        if is_prime(&candidate, size_in_bits) {
-            candidate;
+        if is_prime(&candidate) {
+            candidate.clone();
         }
-        candidate += 1;
+        candidate += BigUint::from_bytes_be(b"1");
     }
 }
 
@@ -64,7 +128,7 @@ impl<T: Storer> SetAccumulator<T> {
     pub fn new(s: T) -> SetAccumulator<T> {
         SetAccumulator { store: s }
     }
-    pub fn add(&self, value: &[u8]) {
+    pub fn add(&mut self, value: &[u8]) {
         // get random once time use byte sequence
         let nonce = rand::thread_rng().gen::<[u8; 32]>();
         // hash the value and nonce concatentated and then map to prime
